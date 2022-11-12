aa174faa989a42a2e7470818b4b7a7de34d8b504
StanislavSapal <sapalstanislav22@gmail.com>
2022-11-12 13:40:42 +0200

    created base template

--- menstoreproject/order/models.py
+++ menstoreproject/order/models.py
@@ -7,7 +7,7 @@ class Order(TimeStampedModel):
     cart = models.ForeignKey('cart.Cart', on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    phone = models.CharField(blank=True)
-    address = models.CharField(blank=True)
+    phone = models.CharField(blank=True, max_length=50)
+    address = models.CharField(blank=True, max_length=250)
     email = models.EmailField(blank=True)
 
