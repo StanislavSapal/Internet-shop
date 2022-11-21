17ebd25108d94b7e3dd0d067ceec3d9aaa63f2da
StanislavSapal <sapalstanislav22@gmail.com>
2022-11-21 11:09:39 +0200

    catalog/views.py modified

--- menstoreproject/catalog/views.py
+++ menstoreproject/catalog/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, CreateView
 from django.shortcuts import render, get_object_or_404, redirect
-
-from catalog.models import *
+from .models import *
 
 
 def index(request):
