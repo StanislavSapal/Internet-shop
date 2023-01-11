6373254cd576316d4ac0bee12469d691abc74fb0
StanislavSapal <sapalstanislav22@gmail.com>
2023-01-11 23:50:56 +0200

    done some modifies for home page view

--- menstoreproject/menstoreproject/views.py
+++ menstoreproject/menstoreproject/views.py
@@ -5,14 +5,10 @@ from catalog.models import Product, Category
 class HomeView(ListView):
     model = Product
     template_name = 'home.html'
-    context_object_name = 'best_products'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Головна MenStore'
         context['categories'] = Category.objects.all()
+        context['best_products'] = Product.objects.filter(top_seller=True)
         return context
-
-    def get_queryset(self):
-        return Product.objects.filter(top_seller=True).select_related('category')
 
