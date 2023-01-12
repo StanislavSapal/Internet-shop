2b34093c12c6ee13fc8bf4af8673ee45a7879887
StanislavSapal <sapalstanislav22@gmail.com>
2023-01-12 14:28:19 +0200

    coutnting added to home view page, home template modified

--- menstoreproject/menstoreproject/views.py
+++ menstoreproject/menstoreproject/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.views.generic import ListView
 from catalog.models import Product, Category
 
@@ -8,7 +9,8 @@ class HomeView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['categories'] = Category.objects.all()
+        context['categories'] = Category.objects.annotate(cnt=Count('product'))
         context['best_products'] = Product.objects.filter(top_seller=True)
         return context
+
 
