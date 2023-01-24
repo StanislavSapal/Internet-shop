4537e6f4ff8402094a61892622a84c675c1acf8e
StanislavSapal <sapalstanislav22@gmail.com>
2023-01-24 15:38:52 +0200

    feature/cart-page titles fixed in templates adminregister decorator used

--- menstoreproject/cart/admin.py
+++ menstoreproject/cart/admin.py
@@ -2,13 +2,12 @@ from django.contrib import admin
 from .models import *
 
 
+@admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
-    list_display = ('user', 'status', 'created_at')
+    list_display = ('id', 'user', 'status', 'created')
 
 
+@admin.register(CartItem)
 class CartItemAdmin(admin.ModelAdmin):
     list_display = ('cart', 'product', 'quantity')
 
-
-admin.site.register(Cart, CartAdmin)
-admin.site.register(CartItem, CartItemAdmin)
--- menstoreproject/cart/models.py
+++ menstoreproject/cart/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
+from menstoreproject.models import TimeStampedModel
 
-class Cart(models.Model):
+
+class Cart(TimeStampedModel):
     CART_STATUS_CHOICES = (
         ('O', 'Open'),
         ('C', 'Closed')
     )
     user = models.ForeignKey('authentication.User', on_delete=models.CASCADE)
     status = models.CharField(choices=CART_STATUS_CHOICES,  max_length=50)
-    created_at = models.DateTimeField(auto_now_add=True)
 
 
-class CartItem(models.Model):
+class CartItem(TimeStampedModel):
     cart = models.ForeignKey('Cart', on_delete=models.CASCADE)
     product = models.ForeignKey('catalog.Product', on_delete=models.SET_NULL, null=True, blank=True)
     quantity = models.IntegerField()
