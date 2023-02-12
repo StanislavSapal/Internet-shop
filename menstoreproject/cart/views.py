31e960c3176ef25d41cd439936d3ec267d71ddd0
StanislavSapal <sapalstanislav22@gmail.com>
2023-02-12 22:13:43 +0200

    useless code deleted from the viewset

--- menstoreproject/cart/views.py
+++ menstoreproject/cart/views.py
@@ -35,12 +35,5 @@ class CartItemViewSet(viewsets.ModelViewSet):
     def get_cart(self):
         return Cart.objects.get(user=self.request.user)
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
     def perform_create(self, serializer):
         serializer.save(cart=self.get_cart())
