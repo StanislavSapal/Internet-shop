f6f0067396422cf0201449d83bafe9f592730704
StanislavSapal <sapalstanislav22@gmail.com>
2023-02-19 13:55:37 +0200

    trying to use Anton's script

--- menstoreproject/menstoreproject/static/js/cart_scripts.js
+++ menstoreproject/menstoreproject/static/js/cart_scripts.js
@@ -1,9 +1,39 @@
 $(document).ready(function() {
-$('button[class*=minus]').click(function() {
-    let url = 'http://127.0.0.1:8000/cart/'
-    $.get(url, function() {
-        let element = document.getElementById('31')
-        alert(element)
-    });
+
+    function getCookie(name) {
+      let cookieValue = null;
+      if (document.cookie && document.cookie !== "") {
+        const cookies = document.cookie.split(";");
+        for (let i = 0; i < cookies.length; i++) {
+          const cookie = cookies[i].trim();
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + "=")) {
+            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+            break;
+          }
+        }
+      }
+      return cookieValue;
+    }
+
+    $('button[class*=delete_button]').click(function() {
+        let url = '/api/v1/cartitems/'
+        let cart_item_id = $(this).parent().parent().attr('id');
+        $.ajax({
+            url: url + cart_item_id + '/',
+            type: 'DELETE',
+            data: {},
+            headers: {
+                "X-CSRFToken": getCookie("csrftoken"),
+            },
+            contentType:'application/json',
+            dataType: 'json',
+            error: function(result){
+                alrert('Error')
+            },
+            success: function(result) {
+                alert('WOW');
+            }
+        });
     });
 });
