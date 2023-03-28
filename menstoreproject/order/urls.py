17fbc5e946b0d9c728787ebf4561b03110006e2d
StanislavSapal <sapalstanislav22@gmail.com>
2023-03-28 16:00:24 +0300

    my-orders button hiden fot anonymous user

--- menstoreproject/order/urls.py
+++ menstoreproject/order/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('checkout/', OrderConfirmationView.as_view(), name='checkout'),
     path('checkout/success/', successful_order_page_view, name='successful_order'),
-    path('orders/', OrderList.as_view(), name='orders'),
+    path('orders/', login_required(OrderList.as_view()), name='orders'),
 ]
