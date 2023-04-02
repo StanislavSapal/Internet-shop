d733e234a566cec9d5d124070f92f100389daf3b
StanislavSapal <sapalstanislav22@gmail.com>
2023-04-02 17:54:20 +0300

    search function implemented

--- menstoreproject/catalog/urls.py
+++ menstoreproject/catalog/urls.py
@@ -5,6 +5,7 @@ from .views import *
 urlpatterns = [
     path('product/<slug:slug>/', ViewProduct.as_view(), name='view_product'),
     path('products/', ProductsView.as_view(), name='list_of_products'),
-    path('products/<slug:category_slug>/', ProductsView.as_view(), name='products_by_category')
+    path('products/<slug:category_slug>/', ProductsView.as_view(), name='products_by_category'),
+    path('search_products/', ProductSearchView.as_view(), name='search-products'),
 ]
 
