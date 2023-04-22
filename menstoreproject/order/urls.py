a50dac03acce2c1a6df102aaf1d8d5a403401b95
StanislavSapal <sapalstanislav22@gmail.com>
2023-04-22 17:01:20 +0300

    order paths edited

--- menstoreproject/order/urls.py
+++ menstoreproject/order/urls.py
@@ -4,6 +4,6 @@ from .views import *
 urlpatterns = [
     path('checkout/', OrderConfirmationView.as_view(), name='checkout'),
     path('checkout/success/', successful_order_page_view, name='successful_order'),
-    path('user_orders/', UserOrderList.as_view(), name='user_orders'),
-    path('all_orders/', OrderList.as_view(), name='all_orders'),
+    path('user-orders/', UserOrderList.as_view(), name='user_orders'),
+    path('all-orders/', OrderList.as_view(), name='all_orders'),
 ]
