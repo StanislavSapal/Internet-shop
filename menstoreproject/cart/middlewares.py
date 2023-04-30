170c30e02e9b58049b01aa1930b12461eebe7eba
StanislavSapal <sapalstanislav22@gmail.com>
2023-04-30 12:06:36 +0300

    middlewares.py covered with type hints

--- menstoreproject/cart/middlewares.py
+++ menstoreproject/cart/middlewares.py
@@ -2,24 +2,26 @@ from .models import Cart
 from django.utils.functional import SimpleLazyObject
 import random
 import string
+from typing import Optional, Callable
+from django.http import HttpRequest, HttpResponse
 
 
-def get_cart_by_user(user):
+def get_cart_by_user(user: str) -> Optional[Cart]:
     cart = Cart.objects.filter(user=user, status=Cart.StatusChoices.open).last()
     return cart
 
 
-def generate_token():
+def generate_token() -> str:
     token = ''.join(random.choice(string.ascii_letters) for i in range(16))
     return token
 
 
-def get_cart_by_token(token):
+def get_cart_by_token(token: str) -> Optional[Cart]:
     cart = Cart.objects.filter(token=token, status=Cart.StatusChoices.open).last()
     return cart
 
 
-def create_cart_for_anonymous_user(request):
+def create_cart_for_anonymous_user(request: HttpRequest) -> Cart:
     new_token = generate_token()
     request.session['token'] = new_token
     cart = Cart(token=new_token)
@@ -28,10 +30,10 @@ def create_cart_for_anonymous_user(request):
 
 
 class CartMiddleware:
-    def __init__(self, get_response):
+    def __init__(self, get_response: Callable[[HttpRequest], HttpResponse]) -> None:
         self.get_response = get_response
 
-    def __call__(self, request):
+    def __call__(self, request: HttpRequest) -> HttpResponse:
         if request.user.is_authenticated:
             if hasattr(request, 'cart'):
                 response = self.get_response(request)
